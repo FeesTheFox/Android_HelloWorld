3e961dbd145e813bd58484c44986682711cb4379
FeesTheFox <feesblock@gmail.com>
2024-03-12 16:41:28 +0300

    Initial commit, creating a Hello World app

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void b_exitClick(View view) {
+        System.exit(0);
+    }
+}
